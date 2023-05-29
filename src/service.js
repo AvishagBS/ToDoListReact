ec0e1fce6db1c876e21e4bbb6b2e02f65cb2cb15
AvishagBS <a0583228516@gmail.com>
2023-05-29 18:32:17 +0300

    client project

--- src/service.js
+++ src/service.js
@@ -1,26 +1,50 @@
 import axios from 'axios';
 
-const apiUrl = "https://localhost:7271"
+//גורם שהניתוב יעבוד תמיד דרך הניתוב כאן
+axios.defaults.baseURL = "https://localhost:7126"
+
+axios.interceptors.response.use(response=>{
+  return response;
+},error=>{
+  console.log(Promise.reject(error));
+})
 
 export default {
   getTasks: async () => {
-    const result = await axios.get(`${apiUrl}/items`)    
+    const result = await axios.get('/items')    
     return result.data;
   },
 
   addTask: async(name)=>{
-    console.log('addTask', name)
-    //TODO
-    return {};
+    
+    const result  = await axios.post('/items',{
+      "id": 0,
+      "name": name,
+      "isComplete": false
+    },{
+      headers: {
+        'Content-Type': 'application/json'
+      }})
+     return result.data;
+     
   },
 
   setCompleted: async(id, isComplete)=>{
-    console.log('setCompleted', {id, isComplete})
-    //TODO
-    return {};
+
+    const result  = await axios.put(`/items/${id}`,{
+      "id": 0,
+      "name": "string",
+      "isComplete": isComplete
+    },{
+      headers: {
+        'Content-Type': 'application/json'
+      }})
+    return result.data;   
   },
 
-  deleteTask:async()=>{
-    console.log('deleteTask')
+  deleteTask:async(id)=>{
+    
+    const result  = await axios.delete(`/items/${id}`)
+    return result.data;
   }
 };
